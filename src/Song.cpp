40934c9982d18f4cc13eab44da817e3d59f9437c
Hadi Faezpour <hfp13469@gmail.com>
2025-10-25 02:29:07 +0330

    Implement Song constructor and display method

--- src/Song.cpp
+++ src/Song.cpp
@@ -1,1 +1,23 @@
 #include "Song.h"
+#include <iostream>
+#include <iomanip>
+
+Song::Song(const string& artist, const string& track, const string& date,
+    const string& genre, double len, const string& topic)
+    : artist_name(artist),
+    track_name(track),
+    release_data(date),
+    genre(genre),
+    length(len),
+    topic(topic) {
+}
+
+void Song::display() const
+{
+    std::cout << "Artist:       " << artist_name << std::endl
+              << "Track:        " << track_name << std::endl
+              << "Release date: " << release_data << std::endl
+              << "Genre:        " << genre << std::endl
+              << "Topic:        " << topic << std::endl
+              << "Length:       " << std::fixed << std::setprecision(2) << length << " min" << std::endl;
+}
