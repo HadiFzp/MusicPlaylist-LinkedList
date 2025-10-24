3c44a7c8a11803c8787e830222dd5574ca939630
Hadi Faezpour <hfp13469@gmail.com>
2025-10-24 14:59:29 +0330

    Add Song.h class for storing song information

--- include/Song.h
+++ include/Song.h
@@ -1,12 +1,22 @@
 #ifndef REPOS_SONG_H
 #define REPOS_SONG_H
 
-
+#include <string>
+using namespace std;
 
 class Song {
+public:
+	string artist_name;
+	string track_name;
+	string release_data;
+	string genre;
+	string topic;
+	double length;
 
+	Song(const string& artist, const string& track, const string& date,
+		const string& genre, double len, const string& topic)
+
+	void display() const;
 };
-
-
 
 #endif 
